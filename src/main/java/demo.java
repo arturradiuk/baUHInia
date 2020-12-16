937d3793816a12405f7c0c572687c9fc3d3d5f6a
arturradiuk <arturradiuk@gmail.com>
2020-12-16 20:48:01 +0200

    One more test has been added.

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,5 +1,17 @@
+import database.Connector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class demo {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        Connector conn = new Connector();
+        Statement stmt = conn.getConnection().createStatement();
+        String sql = "select * from test where _name = 'lolek'";
+        ResultSet rs = stmt.executeQuery(sql);
+        rs.next();
+        System.out.println(rs.getString("_name"));
+        System.out.println(rs.getString("phone"));
     }
 }
