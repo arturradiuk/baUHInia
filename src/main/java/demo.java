bde2eabe62a94fd2f591105e4fae5226a3b896aa
arturradiuk <arturradiuk@gmail.com>
2020-12-16 19:00:52 +0200

    Test for Connector has been created.

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,15 +1,5 @@
-import database.Connector;
-
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-
 public class demo {
-    public static void main(String[] args) throws SQLException, IOException {
-        Connector connector = new Connector();
-        Connection connection = connector.getConnection();
-        connection.close();
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
