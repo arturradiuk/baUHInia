4747a88e0a17f26b6279376615a288171edcbbb9
arturradiuk <arturradiuk@gmail.com>
2020-12-16 20:48:01 +0200

    One more test has been added.

--- src/test/java/databasetest/ConnectorTest.java
+++ src/test/java/databasetest/ConnectorTest.java
@@ -5,6 +5,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectorTest {
     @Test
@@ -13,6 +16,27 @@ public class ConnectorTest {
         Connection connection = connector.getConnection();
         Assert.assertNotNull(connection);
         connector.close();
+    }
+
+    @Test
+    public void selectTest() {
+        Connector conn = new Connector();
+        Statement stmt = null;
+        try {
+            stmt = conn.getConnection().createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        String sql = "select * from test where _name = 'lolek'";
+        ResultSet rs = null;
+        try {
+            rs = stmt.executeQuery(sql);
+            rs.next();
+            Assert.assertEquals("lolek", rs.getString("_name"));
+            Assert.assertEquals("0000", rs.getString("phone"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 }
