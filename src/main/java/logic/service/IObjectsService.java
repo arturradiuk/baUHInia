a3e05fa1ee3e5c452fe3755d8afe143668b48d12
rempek99 <arkadiusz.remplewicz@gmail.com>
2020-12-18 20:51:58 +0100

    ObjectsManager not tested

--- src/main/java/logic/service/IObjectsService.java
+++ src/main/java/logic/service/IObjectsService.java
@@ -4,10 +4,10 @@ import java.util.Dictionary;
 import java.util.List;
 import java.util.UUID;
 
-public interface IObslugaObiektow {
+public interface IObjectsService {
     void addObject(String name, Dictionary<String,String> parameterSet) throws RepositoryException;
-    void removeObject(UUID objectID);
-    void updateParameters(UUID objectID, Dictionary<String,String> newParametersSet);
+    void removeObject(UUID objectID) throws RepositoryException;
+    void updateParameters(UUID objectID, Dictionary<String,String> newParametersSet) throws RepositoryException;
     List<Dictionary<String,String>> getInfo();
 
 }
--- src/main/java/logic/service/ObjectsManager.java
+++ src/main/java/logic/service/ObjectsManager.java
@@ -1,46 +1,66 @@
 package logic.service;
 
 import database.IAdminData;
-import database.model.ObiektUmieszczalny;
+import database.model.PlacableObject;
 
-import java.util.Dictionary;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
-public class ManagerObiektow implements IObslugaObiektow{
-    private List<ObiektUmieszczalny> obiekty;
+public class ObjectsManager implements IObjectsService {
+    private List<PlacableObject> cachedObjects;
     private IAdminData adminData;
 
-    public ManagerObiektow() {
+    public ObjectsManager() {
         // todo adminData = x
-        aktualizujObiekty();
+        cacheObjects();
     }
 
     @Override
     public void addObject(String name, Dictionary<String, String> parameterSet) throws RepositoryException {
-        ObiektUmieszczalny obj = new ObiektUmieszczalny(UUID.randomUUID(), name, parameterSet);
-        if(obiekty.contains(obj)){
+        PlacableObject obj = new PlacableObject(UUID.randomUUID(), name, parameterSet);
+        if(cachedObjects.contains(obj)){
             throw new RepositoryException(RepositoryException.EXIST);
         }
         adminData.addObject(obj);
-        aktualizujObiekty();
+        cacheObjects();
     }
 
     @Override
-    public void removeObject(UUID objectID) {
+    public void removeObject(UUID objectID) throws RepositoryException {
+        PlacableObject obj = new PlacableObject(objectID,null,null);
+        if(cachedObjects.contains(obj)) {
+            adminData.removeObject(objectID);
+            cacheObjects();
+        }
+        else
+            throw new RepositoryException(RepositoryException.NOT_EXIST);
 
     }
 
     @Override
-    public void updateParameters(UUID objectID, Dictionary<String, String> newParametersSet) {
-
+    public void updateParameters(UUID objectID, Dictionary<String, String> newParametersSet) throws RepositoryException {
+        PlacableObject obj = null;
+        for(PlacableObject o : cachedObjects)
+        {
+            if(o.getId() == objectID){
+                obj = o;
+            }
+        }
+        if(obj == null)
+            throw new RepositoryException(RepositoryException.NOT_EXIST);
+        obj.setParametersSet(newParametersSet);
+        adminData.updateObject(objectID, obj);
+        cacheObjects();
     }
 
     @Override
     public List<Dictionary<String, String>> getInfo() {
-        return null;
+        List<Dictionary<String,String>> output = new ArrayList<>();
+        for(PlacableObject o : cachedObjects){
+            output.add(o.getInfo());
+        }
+        return output;
     }
-    void aktualizujObiekty(){
-        obiekty = adminData.pobierzObiekty();
+    void cacheObjects(){
+        cachedObjects = adminData.getObjects();
     }
 }
