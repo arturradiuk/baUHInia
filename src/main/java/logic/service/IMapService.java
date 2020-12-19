35bad8dabc966081f220b3edbab66ec4f1aaf687
Shykial <szymon.jeziorski@interia.pl>
2020-12-19 16:59:56 +0100

    changing Dictionary type to Map, minor other changes

--- src/main/java/logic/service/IMapService.java
+++ src/main/java/logic/service/IMapService.java
@@ -1,14 +1,14 @@
 package logic.service;
 
 import java.awt.*;
-import java.util.Dictionary;
+import java.util.Map;
 import java.util.UUID;
 
 public interface IMapService {
-    void addMap(String name, Dictionary<Point, UUID> objectsSet, Dictionary<String,String> permissionsSet) throws RepositoryException;
+    void addMap(String name, Map<Point, UUID> objectsSet, Map<String,String> permissionsSet) throws RepositoryException;
     void removeMap(UUID mapID) throws RepositoryException;
-    void updateMap(UUID mapID, Dictionary<Point,UUID> newObjectSet) throws RepositoryException;
-    void updatePermissions(UUID mapID, Dictionary<String,String> newPermissionsSet) throws RepositoryException;
+    void updateMap(UUID mapID, Map<Point,UUID> newObjectSet) throws RepositoryException;
+    void updatePermissions(UUID mapID, Map<String,String> newPermissionsSet) throws RepositoryException;
     String getInfo();
     void updateHeatSimulationFactor(double newValue);
 }
--- src/main/java/logic/service/MapManager.java
+++ src/main/java/logic/service/MapManager.java
@@ -2,11 +2,10 @@ package logic.service;
 
 import database.IAdminData;
 import database.model.MapObject;
-import database.model.PlacableObject;
 
 import java.awt.*;
-import java.util.Dictionary;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class MapManager implements IMapService {
@@ -19,9 +18,9 @@ public class MapManager implements IMapService {
     }
 
     @Override
-    public void addMap(String name, Dictionary<Point, UUID> objectsSet, Dictionary<String, String> permissionsSet) throws RepositoryException {
-        MapObject map = new MapObject(UUID.randomUUID() ,name,objectsSet,permissionsSet);
-        if(cachedMaps.contains(map)){
+    public void addMap(String name, Map<Point, UUID> objectsSet, Map<String, String> permissionsSet) throws RepositoryException {
+        MapObject map = new MapObject(UUID.randomUUID(), name, objectsSet, permissionsSet);
+        if (cachedMaps.contains(map)) {
             throw new RepositoryException(map.toString(), RepositoryException.EXIST);
         }
         adminData.addMap();
@@ -30,51 +29,50 @@ public class MapManager implements IMapService {
 
     @Override
     public void removeMap(UUID mapID) throws RepositoryException {
-        MapObject map = new MapObject(mapID,null,null,null);
-        if(cachedMaps.contains(map)){
+        MapObject map = new MapObject(mapID, null, null, null);
+        if (cachedMaps.contains(map)) {
             adminData.removeMap(mapID);
             cacheMaps();
-        }
-        else
+        } else
             throw new RepositoryException(mapID.toString(), RepositoryException.NOT_EXIST);
     }
 
     @Override
-    public void updateMap(UUID mapID, Dictionary<Point, UUID> newObjectSet) throws RepositoryException {
+    public void updateMap(UUID mapID, Map<Point, UUID> newObjectSet) throws RepositoryException {
         MapObject map = null;
-        for(MapObject m : cachedMaps){
-            if(m.getId() == mapID){
+        for (MapObject m : cachedMaps) {
+            if (m.getId() == mapID) {
                 map = m;
                 break;
             }
         }
-        if(map == null)
+        if (map == null)
             throw new RepositoryException(mapID.toString(), RepositoryException.NOT_EXIST);
-         map.setObjectsSet(newObjectSet);
-         adminData.updateMap(mapID,map);
-         cacheMaps();
+        map.setObjectsSet(newObjectSet);
+        adminData.updateMap(mapID, map);
+        cacheMaps();
     }
 
     @Override
-    public void updatePermissions(UUID mapID, Dictionary<String, String> newPermissionsSet) throws RepositoryException {
+    public void updatePermissions(UUID mapID, Map<String, String> newPermissionsSet) throws RepositoryException {
         MapObject map = null;
-        for(MapObject m : cachedMaps){
-            if(m.getId() == mapID){
+        for (MapObject m : cachedMaps) {
+            if (m.getId() == mapID) {
                 map = m;
                 break;
             }
         }
-        if(map == null)
+        if (map == null)
             throw new RepositoryException(mapID.toString(), RepositoryException.NOT_EXIST);
         map.setPermissionsSet(newPermissionsSet);
-        adminData.updateMap(mapID,map);
+        adminData.updateMap(mapID, map);
         cacheMaps();
     }
 
     @Override
     public String getInfo() {
         StringBuilder stringBuilder = new StringBuilder();
-        for(MapObject m : cachedMaps) {
+        for (MapObject m : cachedMaps) {
             stringBuilder.append(m.toString());
         }
         return stringBuilder.toString();
