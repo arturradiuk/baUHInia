91c21fde17d98eb11a34114ef6eb8fe1bd917412
Dominik Karski <dominik-karski@wp.pl>
2020-12-19 18:49:10 +0100

    login module package.

---
+++ src/main/java/login/UserData.java
@@ -0,0 +1,45 @@
+package login;
+
+public class UserData {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserType type;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public UserData(String firstName, String lastName, String email, String password, UserType type) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+
+    public UserData(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.type = UserType.General;
+    }
+}
