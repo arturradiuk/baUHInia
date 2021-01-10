e6c596e3cd82264345bbb9288cc5b195c8d94e97
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-01-10 13:25:19 +0100

    Updated java.logic.service.MapManager, 'import' changes in kotlin.maps.IMapsService/MapsService

--- src/main/java/logic/service/IMapService.java
+++ src/main/java/logic/service/IMapService.java
@@ -1,13 +1,18 @@
 package logic.service;
 
+import maps.api.MapObject;
+
 import java.awt.*;
 import java.util.Map;
 import java.util.UUID;
+import java.util.List;
 
 public interface IMapService {
-    void addMap(String name, Map<Point, UUID> objectsSet, Map<String,String> permissionsSet) throws RepositoryException;
+    //void addMap(String name, Map<Point, UUID> objectsSet, Map<String,String> permissionsSet) throws RepositoryException;
+    void addMap(String name, int size, List<MapObject> objects);
     void removeMap(UUID mapID) throws RepositoryException;
-    void updateMap(UUID mapID, Map<Point,UUID> newObjectSet) throws RepositoryException;
+    //void updateMap(UUID mapID, Map<Point,UUID> newObjectSet) throws RepositoryException;
+    void updateMap(UUID mapID, List<MapObject> newObjects);
     void updatePermissions(UUID mapID, Map<String,String> newPermissionsSet) throws RepositoryException;
     String getInfo();
     void updateHeatSimulationFactor(double newValue);
