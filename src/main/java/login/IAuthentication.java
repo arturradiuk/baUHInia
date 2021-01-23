499fe2f7f6586a1606c331483c0e20034f6ea1a1
Isseii <sebastian.kujawski.98@poczta.fm>
2021-01-23 22:16:29 +0100

    Addes function of checking currently logged user type

--- src/main/java/login/IAuthentication.java
+++ src/main/java/login/IAuthentication.java
@@ -9,4 +9,6 @@ public interface IAuthentication {
     boolean register(UserData userData) throws IncorrectDataException;
 
     boolean registerAsAdmin(String adminEmail, String adminPassword, UserData userData) throws IncorrectDataException, UserNotFoundException;
+
+    UserType getCurrentUserType();
 }
