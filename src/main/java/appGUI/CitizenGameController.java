c0ede6958cfebf5025a9fa565155325c4ac34fa1
tomdoo77 <tomdoo79@gmail.com>
2021-01-23 14:23:33 +0100

    First test implementation

--- src/main/java/appGUI/CitizenGameController.java
+++ src/main/java/appGUI/CitizenGameController.java
@@ -8,13 +8,35 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Window;
+
 
 public class CitizenGameController implements Initializable {
+
+    @FXML
+    private AnchorPane game_view;
+
+    @FXML
+    private AnchorPane new_game_view;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    public void newGame() {
+        game_view.setVisible(false);
+        game_view.setManaged(false);
+        new_game_view.setVisible(true);
+        new_game_view.setManaged(true);
+
+    }
+
+
 
     @FXML
     public void importMap() {
--- src/main/java/appGUI/Main.java
+++ src/main/java/appGUI/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
             stage = primaryStage;
             Parent root = FXMLLoader.load(
                     Objects.requireNonNull(
-                            getClass().getClassLoader().getResource("scenes/login.fxml"))
+                            getClass().getClassLoader().getResource("scenes/citizen_game.fxml"))
             );
             
             setView(root);
