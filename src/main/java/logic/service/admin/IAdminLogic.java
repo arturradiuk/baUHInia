4833c5e462d14658cd60bb97ca29ed121f5e2983
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-01-24 19:08:01 +0100

    Interface

--- src/main/java/logic/service/admin/IAdminLogic.java
+++ src/main/java/logic/service/admin/IAdminLogic.java
@@ -3,16 +3,29 @@ package logic.service.admin;
 import common.enums.CellType;
 import common.enums.ObjectType;
 import maps.api.Cell;
+import maps.api.Map;
+import maps.api.MapObject;
 
 import java.util.Dictionary;
+import java.util.List;
 import java.util.UUID;
 
 public interface IAdminLogic {
 
-    void addNewMap();
+    void createNewMapTemplate();
+    List<Map> getMapTemplates();
+    List<Map> getAllMaps();
+    List<MapObject> getAllObjects();
     void removeMap(UUID mapID) throws AdminException;
-    void updateMap(UUID mapID, Dictionary<Cell, UUID> cellMapObjectID) throws AdminException;
-    void addNewMapObject(String name, ObjectType type, int length, int width, int height) throws AdminException;
+    // bierze template, naklada obiekty, przypisuje user
+    void createMap(UUID mapTemplateID, UUID userID, List<MapObjectInfo> objectInfos) throws AdminException;
+    void addNewMapObject(String name, int width, int length, int height,
+                         ObjectType type, CellType allowedTerrainType,
+                         int price, double heatFactor) throws AdminException;
     void removeMapObject(UUID mapObjectID) throws AdminException;
-    void updateMapObject(UUID mapObjectID, String name, CellType type,int length,  int width, int height) throws AdminException;
+    void updateMapObject(UUID mapObjectID, String name, int width, int length, int height,
+                         ObjectType type, CellType allowedTerrainType,
+                         int price, double heatFactor) throws AdminException;
+    void switchLockCell(UUID mapID, int cellX, int cellY) throws AdminException;
+    void updateCellType(UUID mapID, int cellX, int cellY, CellType cellType) throws AdminException;
 }
